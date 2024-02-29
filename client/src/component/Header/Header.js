d78c4c5c0af8986d968e7056add5b8f81ff590aa
shubham70500 <sk7050042729@gmail.com>
2024-02-29 17:19:06 +0530

    work completed

--- client/src/component/Header/Header.js
+++ client/src/component/Header/Header.js
@@ -1,15 +1,27 @@
-import React, { useState } from "react"
+import React, { useContext, useState } from "react"
 import "./Header.css"
  import logo from "../image/logo.png"
-import { Link } from "react-router-dom"
+import { Link, } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import { userLoginObj } from "../../contextApi/UserContext"
 
 
 const Header = () => {
+  let {loginStatus,setLoginStatus}=useContext(userLoginObj)
   const [Mobile, setMobile] = useState(false)
+  let navigate=useNavigate()
 
   function mobileMenu(){
     setMobile(!Mobile)
   }
+  const handleLogout=()=>{
+     
+    let token = localStorage.getItem("token");
+    localStorage.removeItem("token",token)
+    setLoginStatus(false)
+    //alert("")
+  }
+
   return (
     <>
       <header>
@@ -31,11 +43,17 @@ const Header = () => {
               <li>
               <Link className="a" to="/about">About</Link>
               </li>
-              <li>
-              <Link className="a" to="/login">Login</Link>
+              
+              {
+                loginStatus ? <li>
+                <Link className="a" onClick={handleLogout}>Logout</Link>
+                </li>:<li>
+                <Link className="a" to="/login">Login</Link>
               </li>
-             
+              }
+              
             </ul>
+          
             <button className='toggle' onClick={mobileMenu}>
               {Mobile ? <i className='fa fa-times'></i> : <i className='fa fa-bars'></i>}
             </button>
